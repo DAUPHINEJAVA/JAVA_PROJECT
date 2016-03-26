bec7f75da72d0aa634288f3d9677119cd5f48e9a
WarsFor42 <mehdi.mouadil@dauphine.eu>
2016-03-26 18:44:31 +0100

    commit Ahmed last

--- AppEJB/ejbModule/app/ejb/DAO/ActionJpaController.java
+++ AppEJB/ejbModule/app/ejb/DAO/ActionJpaController.java
@@ -1,5 +1,50 @@
 package app.ejb.DAO;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
+
+import app.jpa.entities.Action;
+import app.jpa.entities.Publication;
+
 public class ActionJpaController {
+
+	public ActionJpaController(UserTransaction utx, EntityManagerFactory emf) {
+		this.utx = utx;
+		this.emf = emf;
+	}
+
+	private UserTransaction utx = null;
+	private EntityManagerFactory emf = null;
+
+	public EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+
+	public List<Action> findAllAction(int maxResults) {
+		EntityManager em = getEntityManager();
+		String query = "SELECT a FROM Action a";
+		try {
+			Query q = em.createQuery(query);
+			q.setMaxResults(maxResults);
+			return q.getResultList();
+		} finally {
+			em.close();
+		}
+	}
+	
+/*	public void CreateAction(Action action){
+		
+		EntityManager em = getEntityManager();
+		
+		
+		
+		
+		
+		
+	}*/
 
 }
