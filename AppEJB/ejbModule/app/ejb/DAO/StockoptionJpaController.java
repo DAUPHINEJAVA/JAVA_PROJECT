7503d9592162368c706c5fa8f01e022e2ab23b3e
WarsFor42 <mehdi.mouadil@dauphine.eu>
2016-03-28 14:03:53 +0200

    DAO

--- AppEJB/ejbModule/app/ejb/DAO/StockoptionJpaController.java
+++ AppEJB/ejbModule/app/ejb/DAO/StockoptionJpaController.java
@@ -1,5 +1,165 @@
 package app.ejb.DAO;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
+
+import app.ejb.DAO.exceptions.NonexistentEntityException;
+import app.ejb.DAO.exceptions.RollbackFailureException;
+import app.jpa.entities.StockOption;
+import app.jpa.entities.User;
+
 public class StockoptionJpaController {
 
+	public StockoptionJpaController(UserTransaction utx, EntityManagerFactory emf) {
+		this.utx = utx;
+		this.emf = emf;
+	}
+
+	private UserTransaction utx = null;
+	private EntityManagerFactory emf = null;
+
+	/**
+	 * getEntityManager() permet de cr�er une EntityManager.
+	 * 
+	 * @return
+	 */
+	public EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+	
+	
+	public void create(StockOption stockOption) throws RollbackFailureException, Exception {
+
+		EntityManager em = null;
+		try {
+			utx.begin();
+			em = getEntityManager();
+			em.persist(stockOption);
+			utx.commit();
+		} catch (Exception ex) {
+			try {
+				utx.rollback();
+			} catch (Exception re) {
+				throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+			}
+			throw ex;
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+
+
+	public void edit(StockOption stockOption) throws NonexistentEntityException, RollbackFailureException, Exception {
+		EntityManager em = null;
+		try {
+			utx.begin();
+			em = getEntityManager();
+			StockOption persistentStockOption = em.find(StockOption.class, stockOption.getIdContrat());
+
+			List<StockOption> allStockOption = findStockOptionEntities();
+
+			for (StockOption StockOptionListToAttach : allStockOption) {
+
+				if (StockOptionListToAttach.getIdContrat() == stockOption.getIdContrat()) {
+
+					destroy(stockOption.getIdContrat());
+					create(stockOption);
+
+				}
+			}
+			utx.commit();
+		} catch (Exception ex) {
+			try {
+				utx.rollback();
+			} catch (Exception re) {
+				throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+			}
+			String msg = ex.getLocalizedMessage();
+			if (msg == null || msg.length() == 0) {
+				int id = stockOption.getIdContrat();
+				if (findStockOption(id) == null) {
+					throw new NonexistentEntityException("The StockOption with id " + id + " no longer exists.");
+				}
+			}
+			throw ex;
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+
+	
+	public void destroy(int id) throws NonexistentEntityException, RollbackFailureException, Exception {
+		EntityManager em = null;
+		try {
+			utx.begin();
+			em = getEntityManager();
+			StockOption stockOption;
+			try {
+				stockOption = em.getReference(StockOption.class, id);
+				stockOption.getIdContrat();
+			} catch (EntityNotFoundException enfe) {
+				throw new NonexistentEntityException("The StockOption with id " + id + " no longer exists.", enfe);
+			}
+
+			em.remove(stockOption);
+			utx.commit();
+		} catch (Exception ex) {
+			try {
+				utx.rollback();
+			} catch (Exception re) {
+				throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+			}
+			throw ex;
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+
+
+	public List<StockOption> findStockOptionEntities() {
+		return findStockOptionEntities(true, -1, -1);
+	}
+
+
+	public List<StockOption> findStockOptionEntities(int maxResults, int firstResult) {
+		return findStockOptionEntities(false, maxResults, firstResult);
+	}
+
+	private List<StockOption> findStockOptionEntities(boolean all, int maxResults, int firstResult) {
+		EntityManager em = getEntityManager();
+		try {
+			CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+			cq.select(cq.from(StockOption.class));
+			Query q = em.createQuery(cq);
+			if (!all) {
+				q.setMaxResults(maxResults);
+				q.setFirstResult(firstResult);
+			}
+			return q.getResultList();
+		} finally {
+			em.close();
+		}
+	}
+
+	public StockOption findStockOption(int id) {
+		EntityManager em = getEntityManager();
+		try {
+			return em.find(StockOption.class, id);
+		} finally {
+			em.close();
+		}
+	}
+	
+	
 }
+
+
+
